0649f6d7a0531f6f68824c19165c76772e9ab3ae
nesou2 <towers.towers.sebastian@gmail.com>
2022-10-01 07:24:20 -0700

    More boilerplate

--- src/bioreactions/data_containers.py
+++ src/bioreactions/data_containers.py
@@ -1,30 +1,24 @@
 
-
-from typing import List
+from typing import List, Tuple
 from utils.data.load import Data
 import numpy as np
 
 
 class Species():
-
     def __init__(self) -> None:
         self.name : str
         self.identifier : str
         self.data : Data
 
 
-class Extrinsics(Species):
-
+class Extrinsics():
     def __init__(self) -> None:
         pass
 
-
 class Reaction():
-
     def __init__(self) -> None:
-        
         self.input : List[Species]
         self.output : List[Species]
-        self.base_rate : np.ndarray
-        self.environmental: List[Extrinsics]
+        self.base_rate : float
+        #self.environmental: List[Tuple[int,Extrinsics]]
 
