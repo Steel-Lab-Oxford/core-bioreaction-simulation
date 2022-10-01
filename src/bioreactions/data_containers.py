36e0d88348e1b1e0ce571d932e01bb5aae7c0cb4
olive004 <olivia.gallup@gmail.com>
2022-10-01 11:59:07 +0100

    extrinsics

--- src/bioreactions/data_containers.py
+++ src/bioreactions/data_containers.py
@@ -1,15 +1,30 @@
 
 
+from typing import List
+from utils.data.load import Data
+import numpy as np
+
+
 class Species():
 
     def __init__(self) -> None:
         self.name : str
         self.identifier : str
-        self.data
+        self.data : Data
+
+
+class Extrinsics(Species):
+
+    def __init__(self) -> None:
+        pass
 
 
 class Reaction():
 
     def __init__(self) -> None:
-        pass
+        
+        self.input : List[Species]
+        self.output : List[Species]
+        self.base_rate : np.ndarray
+        self.environmental: List[Extrinsics]
 
