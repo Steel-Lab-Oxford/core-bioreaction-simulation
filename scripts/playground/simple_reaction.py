ef7806c4b257e58b16e6c8afecc578f1e792d8c2
olive004 <olivia.gallup@gmail.com>
2022-10-20 18:37:50 +0100

    bug: fix species creation

--- scripts/playground/simple_reaction.py
+++ scripts/playground/simple_reaction.py
@@ -2,6 +2,7 @@
 
 import logging
 import sys
+from typing import List
 
 import chex
 import numpy
@@ -14,10 +15,13 @@ from scripts.playground.misc import flatten_listlike, load_json_as_dict
 
 
 def main():
-    logging.info('Activating logger') 
+    logging.info('Activating logger')
 
     def create_combined_species(*species: str):
-        return tuple(species)
+        return [Species(tuple(species))]
+
+    def make_species(species: List[str]):
+        return list(Species(i) for i in sorted(species))
 
     def construct_model(config: dict):
         model = BasicModel()
@@ -29,12 +33,21 @@ def main():
             output = [None] * len(input)
         for i, o in zip(input, output):
             reaction = Reaction()
-            reaction.input = sorted(i)
-            reaction.output = create_combined_species(*i) if o is None else tuple(o)
+            reaction.input = make_species(i)
+            reaction.output = create_combined_species(
+                *i) if o is None else make_species(i)
             model.reactions.append(reaction)
 
-        model.species = set(list(set(flatten_listlike([r.input for r in model.reactions])))
-        + list(set([r.output for r in model.reactions])) )
+        species = [s for s in [r.input + r.output for r in model.reactions]]
+        species = [s for s in model.reactions.input + model.reactions.output]
+
+        species_names = list(set(
+            list(set(flatten_listlike([r.input for r in model.reactions])))
+            + list(set([r.output for r in model.reactions]))))
+        model.species = list(Species(i) for i in species_names)
+        model.species = list(set(
+            list(set(flatten_listlike([r.input for r in model.reactions])))
+            + list(set([r.output for r in model.reactions]))))
         return model
 
     config = load_json_as_dict('./scripts/playground/simple_config.json')
@@ -45,12 +58,12 @@ def main():
 
     ##
 
-    def create_reactions_from_model(model : BasicModel, config: dict):
+    def create_reactions_from_model(model: BasicModel, config: dict):
         qreactions = QuantifiedReactions()
         qreactions.init_properties(model, config)
 
         reactions = Reactions()
-        reactions.inputs = qreactions.quantities 
+        reactions.inputs = qreactions.quantities
         reactions.output_rates = chex.array(config.get('output_rates'))
         return qreactions
 
