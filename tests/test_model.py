53a8afcddbf5c26f4cfbfa7bd81361c30c1b33c2
olive004 <olivia.gallup@gmail.com>
2022-10-25 18:39:42 +0100

    tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,6 @@
 
 import unittest
-from bioreaction.model.data_containers import Species
+from bioreaction.model.data_containers import QuantifiedReactions, Species
 from scripts.playground.simple_reaction import construct_model
 
 
@@ -20,6 +20,33 @@ class TestModel(unittest.TestCase):
 
         # TODO: write a test that makes sure that each component within the output Species is also a Species
         # and that that Species is the same object as the one in the inputs list
+
+    def test_species_concentrations(self):
+        fake_config = {
+            "reactions": {
+                'inputs': [['A'], ['A', 'B']],
+                'outputs': [['A_degraded'], ['AB']]
+            }, 
+            "starting_concentration": {
+                "A": 10,
+                "B": 1,
+                "A_degraded": 0,
+                "AB": 1
+            },
+            "forward_rates": [
+                1.5, 1.4
+            ],
+            "reverse_rates": [
+                0, 0.5
+            ]
+        }
+
+        model = construct_model(fake_config)
+        qreactions = QuantifiedReactions()
+        qreactions.init_properties(model, fake_config)
+
+        self.assertEquals(qreactions.quantities[0], 
+            fake_config['starting_concentration']['A'])
 
 
 if __name__ == '__main__':
