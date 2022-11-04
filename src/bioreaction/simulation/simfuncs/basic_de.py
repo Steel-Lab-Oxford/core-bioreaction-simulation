f1a6353ea6f5ead72cd1347834041b3a50264c03
olive004 <olivia.gallup@gmail.com>
2022-11-04 16:59:31 +0000

    updating sdeterministic

--- src/bioreaction/simulation/simfuncs/basic_de.py
+++ src/bioreaction/simulation/simfuncs/basic_de.py
@@ -4,20 +4,22 @@ import jax.numpy as jnp
 import chex
 
 
-def one_step_de_sim(spec_conc: chex.ArrayDevice, reactions: Reactions, delta_t: float):
+def one_step_de_sim(spec_conc, reactions):
     concentration_factors_in = jnp.prod(
         jnp.power(spec_conc, (reactions.inputs)), axis=1)
     concentration_factors_out = jnp.prod(
         jnp.power(spec_conc, (reactions.outputs)), axis=1)
     forward_delta = concentration_factors_in * reactions.forward_rates
     reverse_delta = concentration_factors_out * reactions.reverse_rates
-    return spec_conc \
-        + ((forward_delta - reverse_delta) @ (reactions.outputs - reactions.inputs) 
-        )  * delta_t
+    return (forward_delta - reverse_delta) @ (reactions.outputs - reactions.inputs) 
+
+
+def one_step_scan_wrapper(spec_conc: chex.ArrayDevice, reactions: Reactions, delta_t: float):
+    return spec_conc + one_step_de_sim(spec_conc, reactions)  * delta_t
 
 
 def basic_de_sim(starting_concentration: chex.ArrayDevice, reactions: Reactions, delta_t: float, num_steps: int):
     def to_scan(carry, inp):
-        step_output = one_step_de_sim(carry, reactions, delta_t)
+        step_output = one_step_scan_wrapper(carry, reactions, delta_t)
         return step_output, step_output
     return jax.lax.scan(to_scan, starting_concentration, None, length=num_steps)
