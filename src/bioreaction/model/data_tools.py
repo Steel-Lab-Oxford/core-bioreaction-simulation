0ccb6f75b9a2d562f3a198c5563472f3d6a66cba
olive004 <olivia.gallup@gmail.com>
2022-11-09 13:07:35 +0000

    added construct func

--- src/bioreaction/model/data_tools.py
+++ src/bioreaction/model/data_tools.py
@@ -10,6 +10,14 @@ def combine_species(species: List[str], ref_species: Dict[str, Species]):
     return [Species(tuple(make_species(species, ref_species)))]
 
 
+def pairup_combination(samples: list, astype=list):
+    combination = []
+    for si in samples:
+        for sj in samples:
+            combination.append(tuple(sorted([si, sj])))
+    return sorted([astype(s) for s in (set(combination))])
+
+
 def make_species(species: List[str], ref_species: Dict[str, Species]):
     return list(ref_species[i] for i in sorted(species))
 
@@ -40,6 +48,26 @@ def construct_model(config: dict):
             i, ref_species) if o is None else make_species(o, ref_species)
         reaction.forward_rate = forward_rates[idx]
         reaction.reverse_rate = reverse_rates[idx]
+        model.reactions.append(reaction)
+
+    model.species = retrieve_species_from_reactions(model)
+    return model
+
+
+def construct_model_fromnames(sample_names):
+
+    model = BasicModel()
+    inputs = sample_names + pairup_combination(sample_names) + [[]] * len(sample_names)
+    outputs = [[]] * len(sample_names) + pairup_combination(sample_names, astype=str) + sample_names
+    ref_species = {s: Species(s) for s in set(
+        flatten_listlike(inputs + outputs, safe=True)) if s is not None}
+    for idx, (i, o) in enumerate(zip(inputs, outputs)):
+        reaction = Reaction()
+        reaction.input = make_species(i, ref_species)
+        reaction.output = combine_species(
+            i, ref_species) if o is None else make_species(o, ref_species)
+        reaction.forward_rate = None
+        reaction.reverse_rate = None
         model.reactions.append(reaction)
 
     model.species = retrieve_species_from_reactions(model)
