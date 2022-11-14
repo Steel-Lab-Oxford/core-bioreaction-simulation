4f0d79f9370c8bc43d5350e4fb3ef5ec1739fba9
olive004 <olivia.gallup@gmail.com>
2022-11-14 17:59:24 +0000

    signal should not be optional

--- src/bioreaction/simulation/simfuncs/basic_de.py
+++ src/bioreaction/simulation/simfuncs/basic_de.py
@@ -11,11 +11,11 @@ def one_step_de_sim(spec_conc, reactions: Reactions):
         jnp.power(spec_conc, (reactions.outputs)), axis=1)
     forward_delta = concentration_factors_in * reactions.forward_rates
     reverse_delta = concentration_factors_out * reactions.reverse_rates
-    return (forward_delta - reverse_delta) @ (reactions.outputs - reactions.inputs) 
+    return (forward_delta - reverse_delta) @ (reactions.outputs - reactions.inputs)
 
 
 def one_step_scan_wrapper(spec_conc: chex.ArrayDevice, reactions: Reactions, delta_t: float):
-    return spec_conc + one_step_de_sim(spec_conc, reactions)  * delta_t
+    return spec_conc + one_step_de_sim(spec_conc, reactions) * delta_t
 
 
 def basic_de_sim(starting_concentration: chex.ArrayDevice, reactions: Reactions, delta_t: float, num_steps: int):
@@ -31,6 +31,6 @@ def bioreaction_sim(t, y, args, reactions, signal, signal_onehot, dt):
                            reactions=reactions) + signal(t) * signal_onehot
 
 
-def bioreaction_sim(t, y, args, reactions, signal, signal_onehot, dt):
+def bioreaction_sim(t, y, args, reactions: Reactions, dt, signal, signal_onehot: jnp.ndarray):
     return one_step_de_sim(spec_conc=y,
-                           reactions=reactions, delta_t=1) + signal(t) * signal_onehot
+                           reactions=reactions) + signal(t) * signal_onehot
