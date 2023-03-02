61e0729e8403eb3bbf7444b06022b71a664ea90b
olive004 <olivia.gallup@gmail.com>
2023-03-02 21:45:31 +0000

    add max steps

--- src/bioreaction/simulation/med_sim.py
+++ src/bioreaction/simulation/med_sim.py
@@ -466,7 +466,7 @@ def basic_de(init_state: MedSimInternelState, model: MedSimInternalModel, params
 
     solver = dfx.Tsit5()
     saveat = dfx.SaveAt(ts=jnp.linspace(params.t_start, params.t_end, 100))
-    return dfx.diffeqsolve(terms, solver, t0=params.t_start, t1=params.t_end, dt0=params.delta_t, y0=init_state, saveat=saveat)
+    return dfx.diffeqsolve(terms, solver, t0=params.t_start, t1=params.t_end, dt0=params.delta_t, y0=init_state, saveat=saveat, max_steps=16**4)
 
 
 def very_basic_de(init_state: MedSimInternelState):
