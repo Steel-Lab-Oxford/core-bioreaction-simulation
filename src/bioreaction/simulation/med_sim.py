caab5f2e94315d9fdbac15f4cc7a54f81362d3d4
Sebastian Towers <towers.towers.sebastian@gmail.com>
2023-03-21 10:29:03 +0000

    no changes tbh

--- src/bioreaction/simulation/med_sim.py
+++ src/bioreaction/simulation/med_sim.py
@@ -70,7 +70,7 @@ class MedSimState:
 @chex.dataclass
 class MedSimParams:
     delta_t: float
-    t_start: float
+    t_start: float = 0
     t_end: float
     poisson_sim_reactions: chex.ArrayDevice
     brownian_sim_reaction: chex.ArrayDevice
